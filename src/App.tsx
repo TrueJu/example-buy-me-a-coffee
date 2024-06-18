982db7c6a356d4526a99a63f5170dc3e7f5454b9
TrueJu <julien33widmer@gmail.com>
2024-06-18 13:24:36 +0200

    Add exchange code

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { NODE_URL, NETWORK, WALLET_CONNECT_PROJECT_ID, APP_TITLE, APP_DESCRIPTIO
 import { Helmet } from "react-helmet";
 import Layout from './Layout';
 import BuyCoffee from './BuyCoffee';
+import { Connex } from '@vechain/connex'
 
 // define wallet connect options only in case a project id has been provided
 const walletConnectOptions = !WALLET_CONNECT_PROJECT_ID ? undefined : {
@@ -19,7 +20,41 @@ const walletConnectOptions = !WALLET_CONNECT_PROJECT_ID ? undefined : {
 // query client for react-query
 const queryClient = new QueryClient()
 
-export default function App() {
+export default async function App() {
+
+    const connexInstance = new Connex({
+        node: 'https://testnet.vechain.org',
+        network: 'test',
+    });
+
+    const exchangeABI ={
+        "type": "function",
+        "name": "exchange",
+        "inputs": [],
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256",
+                "internalType": "uint256"
+            }
+        ],
+        "stateMutability": "payable"
+    };
+
+    const clause = connexInstance.thor
+        //Contract address
+        .account('0x6c09952e7e8cd483ee3b57b4c468c9cfa59aff34')
+        .method(exchangeABI)
+        .value('100')
+        .asClause();
+
+    const result = await connexInstance.vendor
+        .sign("tx", [clause])
+        .request();
+    
+    console.log(result);
+
+
     return (
         <Providers>
             <Helmet>
